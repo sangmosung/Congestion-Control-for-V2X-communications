ced590968d758c2e2c5e739aca940ad6dd04c8f5
Taekyung Kim <taekyungkim@Taekyungs-MacBook-Pro.local>
2022-10-12 19:19:05 +0900

    working on function

--- ITT_function.cc
+++ ITT_function.cc
@@ -2,19 +2,21 @@
 
 // ITT는 기존에 설정된 ITT값을 넣는다.
 // TODO: ITT값을 기존의 코드에서 어떤것으로 넣어야 할지 결정해야 한다. 
-int mod_ITT  (int ITT) {
+// Solution(Broadcast): ITT = (0.02*n + 0.015) + (0.02*n + 0.01);
+int mod_ITT  (float ITT) {
     // 내부에서 정의되어야 하는 함수는 CBR, VD, X의 값이다. 
-    int CBR;
+    float CBR;
     int VD;
-    int X;  // X = CBR x VD
-    int a;  // a는 변경된 ITT의 값(최종 출력할 숫자)
+    float X;  // X = CBR x VD
+    float a;  // a는 변경된 ITT의 값(최종 출력할 숫자)
 
     // CBR = 메시지를 전부 보내는 시간 / 1초(설정된 관측시간)
-    // TODO: 메시지 보내는 시간 결정, 
+    // TODO: 메시지 보내는 시간 어떤것을 받아올 것인지?, 관측시간은 1초로 설정하면 괜찮은지 확인 필요
     CBR = 0.8;
 
     // 한 그룹내의 차량 대수 or 총 차량 대수(10~30)
-    VD = 10;
+    // TODO: 차량 대수를 결정하는 함수를 어떤것을 받아올 것인가?
+    VD = (MAX_NODE - 1);  //  RSU node 1대 제외
 
     X = CBR * VD;
 
